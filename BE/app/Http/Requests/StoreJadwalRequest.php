0cd3fb3ab114efca375b7bb02b1af58a2e76f3c7
VTanujaya <vtanujaya6@gmail.com>
2025-11-19 18:35:39 +0700

    Update StoreJadwalRequest.php

--- BE/app/Http/Requests/StoreJadwalRequest.php
+++ BE/app/Http/Requests/StoreJadwalRequest.php
@@ -27,12 +27,21 @@ class StoreJadwalRequest extends FormRequest
             'waktu_mulai'=> 'required',
             'sesi'=> 'required',
             'tahun_ajaran'=> 'required',
-            'id_prodi'=> 'required',
-            'id_makul'=> 'required',
-            'id_dosen'=> 'required',
-            'id_laboran'=> 'required',
+            'id_prodi'=> ['required'],
+            'id_makul'=> ['required'],
+            'id_dosen'=> ['required',
+            Rule::unique('jadwals','id_dosen')
+            ->where('tahun_ajaran', $this->input('tahun_ajaran'))
+            ->where('hari_jadwal', $this->input('hari_jadwal'))
+            ->where('waktu_mulai', $this->input('waktu_mulai'))],
+            'id_laboran'=> ['required',
+            Rule::unique('jadwals','id_laboran')
+            ->where('tahun_ajaran', $this->input('tahun_ajaran'))
+            ->where('hari_jadwal', $this->input('hari_jadwal'))
+            ->where('waktu_mulai', $this->input('waktu_mulai'))],
             'id_ruangan'=> ['required',
             Rule::unique('jadwals','id_ruangan')
+            ->where('tahun_ajaran', $this->input('tahun_ajaran'))
             ->where('hari_jadwal', $this->input('hari_jadwal'))
             ->where('waktu_mulai', $this->input('waktu_mulai'))]
         ];
@@ -40,7 +49,9 @@ class StoreJadwalRequest extends FormRequest
     public function messages()
     {
         return [
-            'id_ruangan.unique' => 'ruangan ini sudah diambil.',
+            'id_dosen.unique' => 'dosen sudah terjadwal di sesi ini.',
+            'id_laboran.unique' => 'laboran sudah terjadwal di sesi ini.',
+            'id_ruangan.unique' => 'ruangan ini sudah digunakan.',
         ];
     }
 }
