6d16d5b5cc892990e38391fd39d20f7693c09b27
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2025-12-02 23:23:17 +0700

    Integrate Dashboard Jadwal features into the API

--- FE/src/interfaces/IFilterDashboard.ts
+++ FE/src/interfaces/IFilterDashboard.ts
@@ -7,3 +7,22 @@ export interface IFilterDashboard {
   laboran: string
   waktuPerkuliahan: string
 }
+
+export interface IJadwalResponse {
+  id: number
+  nama_dosen_1?: string
+  nama_dosen_2?: string
+  nama_laboran?: string
+  nama_prodi?: string
+  nama_makul?: string
+  hari_jadwal?: string
+  waktu_mulai?: string
+  waktu_selesai?: string
+  kode_ruangan?: string
+  kelas?: string
+  status?: string
+  jenis_jadwal?: string
+  nama_periode?: string
+  tahun_periode?: string
+  periodeTahunAjaran?: string
+}
